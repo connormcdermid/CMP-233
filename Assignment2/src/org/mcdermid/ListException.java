004aa7fd087dc3ed567355bc22fdfbb1ccaa23f6
Connor McDermid <connor.mcdermid@mcvts.org>
2022-03-29 20:10:39 -0400

    Added JavaDoc
    Signed-off-by: Connor McDermid <connor.mcdermid@mcvts.org>

--- Assignment2/src/org/mcdermid/ListException.java
+++ Assignment2/src/org/mcdermid/ListException.java
@@ -1,13 +1,27 @@
 package org.mcdermid;
 
+/**
+ * For use with {@link org.mcdermid.ListInterface} and relevant classes
+ */
 public class ListException extends Exception {
 
+    /**
+     * Message to be returned with exception
+     */
     private String message;
 
+    /**
+     * Set message on throw
+     * @param m message
+     */
     public ListException(String m) {
         this.message = m;
     }
 
+    /**
+     * Returns the message
+     * @return message
+     */
     public String toString() {
         return message;
     }
