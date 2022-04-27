f308812df867b55d61b2a4b56edc21acb23c6347
Connor McDermid <connor.mcdermid@mcvts.org>
2022-04-27 12:58:08 -0400

    Rewrote Stack()
    Rewrote Stack entirely such that it no longer uses an LList object
    internally to store elements. Instead, it implements its own version of
    a LinkedList with no tail element.
    
    Signed-off-by: Connor McDermid <connor.mcdermid@mcvts.org>

--- Assignments/src/org/mcdermid/Stack.java
+++ Assignments/src/org/mcdermid/Stack.java
@@ -2,65 +2,47 @@ package org.mcdermid;
 
 public class Stack<T> implements StackInterface<T> {
 
-    private LList<T> items;
+    private Node<T> head; // this internally uses an LList, but it doesn't need a tail.
+                            // because stacks are LIFO
 
+    // private int counter; // shouldn't need a counter
 
-    /**
-     * Constructor to make a Stack already populated with items
-     * @param objs An LList of type T items
-     */
-    public Stack(LList<T> objs) {
-        this.items = objs;
+    public Stack(Node<T> h) {
+        this.head = h;
     }
 
-    /**
-     * Constructor to initialise your Stack with one value.
-     * @param obj One object of type T
-     */
     public Stack(T obj) {
-        this.items = new LList<>(obj);
+        this.head = new Node<>(obj, null, null);
     }
 
-    /**
-     * Default constructor to initialise an empty Stack.
-     */
     public Stack() {
-        this.items = new LList<>();
+        this.head = null;
     }
 
-    /**
-     * Push an item on to the stack.
-     * @param obj One object of type T to be pushed to the stack
-     */
+    public Node<T> getHead() {
+        return this.head;
+    }
+
     public void push(T obj) {
-        if (this.items.getHead() != null) {
-            Node<T> newNode = new Node<>(obj, this.items.getHead(), null);
-            this.items.getHead().setPrev(newNode);
-            this.items.setHead(newNode);
+        if (this.head == null) {
+            this.head = new Node<>(obj, null, null);
         } else {
-            this.items.setHead(new Node<>(obj, null, null));
+            Node<T> newnode = new Node<>(obj, this.head, null);
+            this.head.setPrev(newnode);
+            this.head = newnode;
         }
     }
 
-    /**
-     * Pops an item from the stack.
-     * @return The popped item
-     */
     public T pop() throws ListException {
-        if (this.items.getHead() == null) {
-            throw new ListException("you fool, you absolute buffoon, there aren't any elements in this stack.");
+        if (this.head == null) {
+            throw new ListException("This list is empty.");
         }
-        T ret = this.items.getHead().getValue();
-        this.items.setHead(this.items.getHead().getNext());
-        this.items.getHead().setPrev(null);
+        T ret = this.head.getValue();
+        this.head = this.head.getNext();
         return ret;
     }
 
-    public T peek() throws ListException {
-        try {
-            return this.items.get(1);
-        } catch(ListException e) {
-            throw new ListException("you fool, you absolute buffoon, there aren't any elements in this stack.");
-        }
+    public T peek() {
+        return this.head == null ? null : this.head.getValue();
     }
 }
